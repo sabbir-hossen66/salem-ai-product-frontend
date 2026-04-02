b20149b2320bd6765affc04eff60c507ee86ae22
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2026-04-02 21:12:02 +0600

    issue fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
-import Navbar from '@/components/layout/Navbar'
-import Hero from '@/components/sections/Hero'
-import About from '@/components/sections/About'
-import Sectors from '@/components/sections/Sectors'
-import WhyChooseUs from '@/components/sections/WhyChooseUs'
-import Footer from '@/components/layout/Footer'
+
 import LandingPage from '@/components/landingPage/page'
 
 export default function Home() {
   return (
     <main>
-      {/* <Navbar /> */}
+   
       <LandingPage />
-      {/* <Hero />
-      <About />
-      <Sectors />
-      <WhyChooseUs />
-      <Footer /> */}
     </main>
   )
 }
